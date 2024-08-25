41136e87541fd808a0ed0562a544db984b8c6d17
Rohit Kumar <rohitverma7754@gmail.com>
2024-08-25 20:47:20 +0530

    first commit

--- ecommerce-frontend/src/pages/login.js
+++ ecommerce-frontend/src/pages/login.js
@@ -16,9 +16,9 @@ export default function Login() {
     try {
       const res = await apiRequest.post(`/api/auth/login`, formData);
       console.log(res);
-
       if (res.status==(200|2001)) {
         const { token, user } = res.data;
+        console.log(res.data);
         localStorage.setItem('token', token);
         localStorage.setItem('role', user.role);
         router.push('/');
