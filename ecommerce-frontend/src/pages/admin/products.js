97ea12ae07563eae29d5e8ceac7fc58eabdf6b94
Rohit Kumar <rohitverma7754@gmail.com>
2024-08-25 18:31:57 +0530

    added base_url

--- ecommerce-frontend/src/pages/admin/products.js
+++ ecommerce-frontend/src/pages/admin/products.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { apiRequest } from '../lib/apiRequest.js';
+import { apiRequest } from '../../lib/apiRequest.js';
 
 export default function AdminProducts() {
   const [products, setProducts] = useState([]);
